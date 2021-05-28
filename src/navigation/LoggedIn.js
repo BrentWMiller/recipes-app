54703373257a4b81cda0f132c9b810771e2ebf60
Brent Miller <brentwmiller93@gmail.com>
2021-05-27 23:42:23 -0400

    🚧 adds base logged in navigation stack

---
+++ src/navigation/LoggedIn.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '~screens/Home';
+import TestScreen from '~screens/Test';
+
+const Stack = createStackNavigator();
+
+const LoggedIn = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+        />
+        <Stack.Screen name="Test" component={TestScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default LoggedIn;
