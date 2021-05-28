330de2e634e723d3c81095f6dd990668532ac03f
Brent Miller <brentwmiller93@gmail.com>
2021-05-27 22:58:05 -0400

    🚧 Adds base typography file

---
+++ src/styles/typography.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+import colors from "./colors";
+import fonts from "./fonts";
+
+const buttonStyles = StyleSheet.create({
+  body: {
+    fontFamily: fonts[500]
+  },
+});
+
+export default buttonStyles;
