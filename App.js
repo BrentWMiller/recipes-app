292901d7f78e30380e983b99b8b114bb75fc90bc
Brent Miller <brentwmiller93@gmail.com>
2021-05-27 22:23:03 -0400

    ✨ Sets up font loading; adds test buttons

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, Pressable, StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { Inter_500Medium, Inter_600SemiBold} from "@expo-google-fonts/inter";
+import * as Font from 'expo-font';
+import buttonStyles from './styles/buttons';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  const _loadFontsAsync = async() => {
+    await Font.loadAsync({
+      'Inter500': Inter_500Medium,
+      'Inter600': Inter_600SemiBold,
+    });
+    setFontsLoaded(true);
+  };
+
+  useEffect(() => {
+    _loadFontsAsync();
+  }, []);
+
+  if (!fontsLoaded) {
+    return (
+      <AppLoading />
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Text style={{...buttonStyles.default}}>Button Example</Text>
+        <Text style={{...buttonStyles.dark}}>Button Example</Text>
+        <Text style={{...buttonStyles.light}}>Button Example</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
