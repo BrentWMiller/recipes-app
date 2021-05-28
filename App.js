8d7fc8bfc56cacffa306312293b7d3fd60e6ec37
Brent Miller <brentwmiller93@gmail.com>
2021-05-27 23:43:27 -0400

    🚧 Adds logged in nav

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { Inter_500Medium, Inter_600SemiBold, Inter_700Bold, Inter_400Regular} from "@expo-google-fonts/inter";
 import * as Font from 'expo-font';
-import BUTTONS from '~styles/buttons';
-import TYPOGRAPHY from '~styles/typography';
+import LoggedIn from '~navigation/LoggedIn';
 
 export default function App() {
   const [fontsLoaded, setFontsLoaded] = useState(false);
@@ -29,24 +26,6 @@ export default function App() {
       <AppLoading />
     );
   } else {
-    return (
-      <SafeAreaView>
-        <StatusBar style="auto" />
-
-        <Pressable>
-          <Text style={[BUTTONS.default]}>Button Example</Text>
-        </Pressable>
-        <Pressable>
-          <Text style={[BUTTONS.dark]}>Button Example</Text>
-        </Pressable>
-        <Pressable>
-          <Text style={[BUTTONS.light]}>Button Example</Text>
-        </Pressable>
-
-        <Text style={[TYPOGRAPHY.heading]}>Heading</Text>
-        <Text style={[TYPOGRAPHY.subheading]}>Subheading</Text>
-        <Text style={[TYPOGRAPHY.body]}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias sint consectetur esse aut magnam iste earum id excepturi. Qui sequi labore, nisi rerum dolores explicabo.</Text>
-      </SafeAreaView>
-    );
+    return LoggedIn();
   }
 }
