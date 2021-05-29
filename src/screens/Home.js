f9490c8d207f68e18f73a1d5b37d9493efcaaa63
Brent Miller <brentwmiller93@gmail.com>
2021-05-29 11:20:22 -0400

    🧐 testing different image styles

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -12,26 +12,31 @@ const HomeScreen = ({navigation}) => {
     {
       id: '1',
       title: 'Delicious Breakfast Favorites',
+      image: require('../../assets/test/recipe.jpg'),
       recipesCount: 4
     },
     {
       id: '2',
       title: 'Lunch and Brunch',
+      image: require('../../assets/test/recipe-alt.jpg'),
       recipesCount: 14
     },
     {
       id: '3',
       title: 'Dinner for Winners',
+      image: require('../../assets/test/recipe.jpg'),
       recipesCount: 26
     },
     {
       id: '4',
       title: 'Delicious Deserts',
+      image: require('../../assets/test/recipe-alt.jpg'),
       recipesCount: 15
     },
     {
       id: '5',
       title: 'Killer Cocktails',
+      image: require('../../assets/test/recipe.jpg'),
       recipesCount: 10
     }
   ];
