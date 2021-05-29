abb027ed40edef31517afe6f425d05fe094f53d8
Brent Miller <brentwmiller93@gmail.com>
2021-05-29 00:22:58 -0400

    🚧 Starts building the home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { Pressable, SafeAreaView, Text } from "react-native";
-import BUTTONS from "~styles/buttons";
+import { SafeAreaView, Text, View } from "react-native";
+import RecipeSearchInput from "~components/RecipeSearchInput";
+import RecipeSearchFilterToggle from "~components/RecipeSearchFilterToggle";
+import APP from "~styles/app";
+import TYPOGRAPHY from "~styles/typography";
 
 const HomeScreen = ({navigation}) => {
   return (
     <SafeAreaView>
-      <Text>Home Screen</Text>
-      <Pressable onPress={() => navigation.navigate('Test')}>
-        <Text style={[BUTTONS.dark]}>Go to test screen</Text>
-      </Pressable>
+      <View style={[APP.container, {paddingTop: 20}]}>
+        <Text style={[TYPOGRAPHY.heading]}>Recipes</Text>
+      </View>
+      <View style={[APP.container, {flexDirection: 'row', marginTop: 24}]}>
+        <RecipeSearchInput />
+        <View style={{ marginLeft: 16}}>
+          <RecipeSearchFilterToggle />
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
