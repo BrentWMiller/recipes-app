df629722a2db9e5de4a8c9b9ae35d6671afb49a2
Brent Miller <brentwmiller93@gmail.com>
2021-05-30 00:47:44 -0400

    💄 adds fun pattern when no image; tweaks size

--- src/components/CategoryBlock.js
+++ src/components/CategoryBlock.js
@@ -3,6 +3,8 @@ import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import COLORS from '~styles/colors';
 import FONTS from '~styles/fonts';
+import Dots from "~svgs/dots.svg";
+import GradientShape from './GradientShape';
 
 const screen = Dimensions.get("screen");
 
@@ -18,10 +20,20 @@ function CategoryBlock(props) {
       overflow: 'hidden',
       position: 'relative',
       borderRadius: 25,
+      width: ((screen.width / 1.25) - (16 * 2)),
+      height: screen.width / 1.5,
+      backgroundColor: COLORS.black
     },
     image: {
-      width: ((screen.width / 1.25) - (16 * 2)),
-      height: screen.width / 1.25,
+      width: '100%',
+      height: '100%',
+    },
+    dots: {
+      position: 'absolute',
+      top: -8,
+      transform: [
+        {rotate: '180deg'}
+      ]
     },
     description: {
       position: 'absolute',
@@ -35,13 +47,18 @@ function CategoryBlock(props) {
   return (
     <View style={styles.container}>
       <View style={styles.wrapper}>
-        <Image
-          style={styles.image}
-          source={category.image}
-        />
+        {
+          category.image
+          ? <Image style={styles.image} source={category.image} />
+          : <>
+              
+              <GradientShape size={200} radius={100} rotation={65}/>
+              <Dots style={styles.dots} width="375" height="140" color={COLORS.gray[900]}/>
+            </>
+        }
 
         <LinearGradient
-          colors={['transparent', 'rgba(0,0,0,0.55)']}
+          colors={category.image ? ['transparent', 'rgba(0,0,0,0.55)'] : ['transparent', 'transparent']}
           style={styles.description}
         >
           <Text style={[{color: COLORS.white, fontSize: 22, fontFamily: FONTS.bold700, marginBottom: 8}]}>{ category.title }</Text>
