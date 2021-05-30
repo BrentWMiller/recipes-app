1572cd166e8b7bf5bf011a9064f4f09f8936a60f
Brent Miller <brentwmiller93@gmail.com>
2021-05-29 22:57:48 -0400

    🚧 Sets up recipe block with most data

--- src/components/RecipeBlock.js
+++ src/components/RecipeBlock.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import COLORS from '~styles/colors';
 import TYPOGRAPHY from '~styles/typography';
+import Placeholder from './Placeholder';
 import TimeFormatted from './TimeFormatted';
 
 function RecipeBlock(props) {
@@ -10,8 +11,10 @@ function RecipeBlock(props) {
   const styles = StyleSheet.create({
     wrapper: {
       overflow: 'hidden',
+      flexDirection: 'row',
+      alignItems: 'center',
       marginTop: first ? 0 : 16,
-      padding: 25,
+      padding: 20,
       borderWidth: 1,
       borderColor: COLORS.gray[200],
       borderRadius: 25,
@@ -21,10 +24,22 @@ function RecipeBlock(props) {
 
   return (
     <View style={styles.wrapper}>
-      <Text style={[TYPOGRAPHY.subheading, {marginBottom: 8}]}>{ recipe.name }</Text>
+      <Placeholder title={ recipe.name } size={ 75 } style={{ marginRight: 16 }}/>
 
-      <TimeFormatted time={recipe.time} style={[TYPOGRAPHY.body, {color: COLORS.gray[500]}]} />
-      <Text style={[TYPOGRAPHY.body, {color: COLORS.gray[500]}]}>{ recipe.ingredients } ingredients</Text>
+      <View style={{ flexShrink: 1 }}>
+        <Text
+          style={[TYPOGRAPHY.subheading, { marginBottom: 8 }]}
+          numberOfLines={ 2 }
+          ellipsizeMode="tail"
+        >
+          { recipe.name }
+        </Text>
+
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <TimeFormatted time={recipe.time} style={[TYPOGRAPHY.body, {color: COLORS.gray[500]}]} />
+          <Text style={[TYPOGRAPHY.body, {color: COLORS.gray[500]}]}>{ recipe.ingredients } ingredients</Text>
+        </View>
+      </View>
     </View>
   );
 }
