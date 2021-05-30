a881a300544185e47faf371a50dc1a98d6f4b178
Brent Miller <brentwmiller93@gmail.com>
2021-05-29 22:55:54 -0400

    ♻️ Replaced func with Placeholder

--- src/components/UserAvatar.js
+++ src/components/UserAvatar.js
@@ -1,33 +1,15 @@
 import React from 'react';
-import { View, Text, Pressable, StyleSheet } from 'react-native';
-import COLORS from '~styles/colors';
-import FONTS from '~styles/fonts';
+import { Pressable } from 'react-native';
+import Placeholder from './Placeholder';
 
 function UserAvatar(props) {
   const { onPress, initials } = props;
 
   return (
-    <Pressable onPress={onPress} style={styles.wrapper}>
-      <Text style={styles.initials}>{ initials.substring(0,2) }</Text>
+    <Pressable onPress={onPress}>
+      <Placeholder title={ initials } />
     </Pressable>
   );
 }
-
-styles = StyleSheet.create({
-  wrapper: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 50,
-    height: 50,
-    backgroundColor: COLORS.black,
-    borderRadius: 15,
-  },
-  initials: {
-    fontSize: 16,
-    fontFamily: FONTS.medium500,
-    textTransform: 'uppercase',
-    color: COLORS.white
-  }
-});
 
 export default UserAvatar;
