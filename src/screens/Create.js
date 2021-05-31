309ee92333fc4d3d988d6aff97d555b3003333a8
Brent Miller <brentwmiller93@gmail.com>
2021-05-31 17:03:46 -0400

    💄 Builds out the login/create screens

--- src/screens/Create.js
+++ src/screens/Create.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import { View, Text, SafeAreaView } from "react-native";
 import AppLink from '~components/AppLink';
+import CreateForm from '~components/CreateForm';
 import DismissKeyboard from '~components/DismissKeyboard';
-import LoginForm from '~components/LoginForm';
 import APP from '~styles/app';
 import TYPOGRAPHY from '~styles/typography';
 
-function LoginScreen(props) {
+function CreateScreen(props) {
   const navigation = props.navigation;
 
   return (
     <DismissKeyboard>
       <SafeAreaView>
         <View style={[APP.container]}>
-          <Text style={[TYPOGRAPHY.heading]}>Sign in</Text>
+          <Text style={[TYPOGRAPHY.heading]}>Create an account</Text>
 
-          <LoginForm />
+          <CreateForm />
 
           <AppLink
-            title="I need to create an account."
+            title="I already have an account."
             style={{ marginTop: 20 }}
-            onPress={() => navigation.navigate('Create')}
+            onPress={() => navigation.navigate('Login')}
           />
         </View>
       </SafeAreaView>
@@ -28,4 +28,4 @@ function LoginScreen(props) {
   );
 }
 
-export default LoginScreen;
+export default CreateScreen;
