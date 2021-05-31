2028eedd259eede13255ce4763bc6c62016b8483
Brent Miller <brentwmiller93@gmail.com>
2021-05-31 17:11:42 -0400

    ♻️ Simplifies the dimiss code

--- src/components/DismissKeyboard.js
+++ src/components/DismissKeyboard.js
@@ -2,12 +2,8 @@ import React from 'react';
 import { Keyboard,  TouchableWithoutFeedback } from 'react-native';
 
 function DismissKeyboard({ children }) {
-  const dismiss = () => {
-    Keyboard.dismiss();
-  };
-
   return (
-    <TouchableWithoutFeedback onPress={() => dismiss()}>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       {children}
     </TouchableWithoutFeedback>
   );
