9f805fab8c415c893bc7d2d73bbb278eca5c01b6
Brent Miller <brentwmiller93@gmail.com>
2021-05-31 12:52:13 -0400

    🚧 sets up base login fields

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { SafeAreaView, ScrollView, Text, View } from 'react-native';
+import { useState } from 'react/cjs/react.development';
 import APP from '~styles/app';
 import TYPOGRAPHY from '~styles/typography';
+import InputWithLabel from './InputWithLabel';
 
 function LoginForm(props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const login = () => {
+    console.log(email, password);
+  }
+  
   return (
     <SafeAreaView>
       <View style={[APP.container]}>
-        <Text style={[TYPOGRAPHY.heading]}>Login</Text>
+        <Text style={[TYPOGRAPHY.heading]}>Sign in</Text>
+
+        <View style={{marginTop: 20}}>
+          <InputWithLabel
+            style={{marginBottom: 16}}
+            label="Email"
+            onChangeText={(text) => setEmail(text)}
+            autoCompleteType="email"
+            autoFocus={true}
+            autoCorrect={false}
+            returnKeyType="next"
+            keyboardType="email-address"
+          />
+          <InputWithLabel
+            label="Password"
+            onChangeText={(text) => setPassword(text)}
+            autoCompleteType="password"
+            autoCorrect={false}
+            secureTextEntry={true}
+            onSubmitEditing={() => login()}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
