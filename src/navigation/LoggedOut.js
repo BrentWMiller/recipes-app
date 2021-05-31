0fa228743c18d2b8aec090690dc53f2078b5ca92
Brent Miller <brentwmiller93@gmail.com>
2021-05-31 17:03:31 -0400

    💄 sets up the logged out view

--- src/navigation/LoggedOut.js
+++ src/navigation/LoggedOut.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '~screens/Login';
+import CreateScreen from "~screens/Create";
 
 const Stack = createStackNavigator();
 
@@ -14,13 +15,8 @@ const LoggedOut = () => {
           cardStyle: { backgroundColor: '#fff' }
         }}
       >
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{
-            headerShown: false
-          }}
-        />
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Create" component={CreateScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
