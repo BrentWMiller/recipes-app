a8248a7cdb6226500a50dfd5a442af2e3f5d9d33
Brent Miller <brentwmiller93@gmail.com>
2021-06-04 22:44:04 -0400

    ✨ Hooks up the ingredient fields to events

--- src/components/IngredientsInputList.js
+++ src/components/IngredientsInputList.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View, Text, Button, Pressable, StyleSheet } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { View, Text, Button, Pressable, StyleSheet, TextInput } from 'react-native';
 import APP from '~styles/app';
 import InputWithLabel from './InputWithLabel';
 import Minus from "~svgs/minus.svg";
@@ -9,7 +9,8 @@ function IngredientsInputList(props) {
   const { title } = props;
   const [ingredients, setIngredients] = useState([{
     id: new Date().getTime(),
-    text: ''
+    quantity: '',
+    name: '',
   }]);
 
   const updateIngredients = (ingredients) => {
@@ -19,7 +20,8 @@ function IngredientsInputList(props) {
   const addIngredient = () => {
     setIngredients([...ingredients, {
       id: new Date().getTime(),
-      text: ''
+      quantity: '',
+      name: '',
     }]);
     updateIngredients(ingredients);
   };
@@ -35,20 +37,14 @@ function IngredientsInputList(props) {
       <Text style={[APP.inputLabel]}>{ title }</Text>
 
       {ingredients.map((ingredient, index) => {
-       return (
-         <View key={ingredient.id} style={{flexDirection: 'row'}}>
-          <InputWithLabel
-            style={{ flexGrow: 1 }}
-            onSubmitEditing={() => addIngredient()}
+        return (
+          <IngredientInput
+            key={ ingredient.id }
+            ingredient={ ingredient }
+            remove={(id) => removeIngredient(id)}
+            add={() => addIngredient()}
           />
-          <Pressable
-            style={[styles.inputButton]}
-            onPress={() => removeIngredient(ingredient.id)}
-          >
-            <Minus width="20" height="3" color={COLORS.black}/>
-          </Pressable>
-        </View>
-       );
+        )
       })}
 
       <Button
@@ -59,8 +55,56 @@ function IngredientsInputList(props) {
   );
 }
 
+const IngredientInput = (props) => {
+  const { ingredient } = props;
+  const nameRef = useRef();
+  const [quantity, setQuantity] = useState(ingredient.quantity);
+  const [name, setName] = useState(ingredient.name);
+
+  return (
+    <View style={{position: 'relative', flexDirection: 'row', marginBottom: 8}}>
+      <TextInput
+        value={ quantity }
+        placeholder="Quantity"
+        style={[APP.input, styles.input, styles.qty, { marginRight: 8}]}
+        blurOnSubmit={false}
+        returnKeyType="next"
+        autoCapitalize="none"
+        onChangeText={(text) => setQuantity(text)}
+        onSubmitEditing={() => nameRef.current.focus()}
+      />
+      <TextInput
+        ref={nameRef}
+        value={ name }
+        placeholder="Label"
+        style={[APP.input, styles.input, styles.name]}
+        autoCapitalize="none"
+        onChangeText={(text) => setName(text)}
+        onSubmitEditing={() => props.add()}
+      />
+
+      <Pressable
+        style={[styles.inputButton]}
+        onPress={() => props.remove(ingredient.id)}
+      >
+        <Minus width="20" height="3" color={COLORS.black}/>
+      </Pressable>
+    </View>
+  )
+}
+
 const styles = StyleSheet.create({
+  input: {
+    flexGrow: 1,
+  },
+  qty: {
+    width: 115,
+    flexGrow: 0
+  },
   inputButton: {
+    position: 'absolute',
+    top: 0,
+    right: 10,
     height: 50,
     width: 40,
     justifyContent: 'center',
