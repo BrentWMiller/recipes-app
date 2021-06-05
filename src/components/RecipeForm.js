ef7ec219716150397234ffd3fe0df414e4f698bb
Brent Miller <brentwmiller93@gmail.com>
2021-06-05 00:08:04 -0400

    ✨ Save recipes in firebase

--- src/components/RecipeForm.js
+++ src/components/RecipeForm.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
-import { useState } from 'react/cjs/react.development';
+import { useDispatch, useSelector } from 'react-redux';
+import { saveRecipe } from '~store/recipes';
 import AppButton from './AppButton';
 import EmojiPicker from './EmojiPicker';
 import IngredientsInputList from './IngredientsInputList';
 import InputWithLabel from './InputWithLabel';
-import RecipeBlock from './RecipeBlock';
 
 function RecipeForm(props) {
+  const dispatch = useDispatch();
+  const uid = useSelector(state => state.userReducer.user.uid);
+
+  const { navigation } = props;
+
   const [name, setName] = useState('');
-  const [emoji, setEmoji] = useState([]);
+  const [emoji, setEmoji] = useState('');
   const [time, setTime] = useState(0);
   const [servings, setServings] = useState(1);
   const [ingredients, setIngredients] = useState([]);
+
+  const submit = async () => {
+    try {
+      await dispatch(saveRecipe({
+        name,
+        emoji,
+        time,
+        servings,
+        ingredients,
+      }, uid))
+
+      navigation.navigate('Home');
+    } catch (error) {
+     alert(error);
+    }
+  }
 
   return (
     <View>
@@ -56,7 +77,7 @@ function RecipeForm(props) {
         title="Ingredients"
         updatedIngredients={(ingredients) => setIngredients(ingredients)}
       />
-      <AppButton title="Save recipe" type="dark" style={{ marginTop: 40 }} onPress={() => console.log('save')} />
+      <AppButton title="Save recipe" type="dark" style={{ marginTop: 40 }} onPress={() => submit()} />
     </View>
   );
 }
--- src/store/recipes.js
+++ src/store/recipes.js
@@ -2,23 +2,27 @@ import { firebase } from '~utils/firebase';
 import { createAction, createReducer } from '@reduxjs/toolkit';
 
 const initialState = {
-  panel: {
-    visible: false,
-    title: 'Add a recipe'
-  }
+  recipes: []
 };
 
 export const recipesReducer = createReducer(initialState, {
-  [createAction('recipes/SET_MODAL_VISIBILITY')]: (state, action) => {
-    state.panel.visible = action.payload;
+  [createAction('recipes/SET_RECIPES')]: (state, action) => {
+    state.recipes = action.payload;
   },
 });
 
-export const setModalVisibility = (visibility) => {
-  return async (dispatch) => {
-    dispatch({
-      type: 'recipes/SET_MODAL_VISIBILITY',
-      payload: visibility,
-    });
+export const saveRecipe = (recipe, uid) => {
+  return async () => {
+    try {
+      await firebase.firestore().collection(`users/${uid}/recipes`).doc().set({
+        ...recipe,
+        created: new Date(),
+        updated: new Date()
+      });
+
+      return;
+    } catch (error) {
+      throw error;
+    }
   }
 } 
