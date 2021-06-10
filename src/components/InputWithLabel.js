8e96b7d1a3e0b405965f65d7bb3ed07afb9a34ae
Brent Miller <brentwmiller93@gmail.com>
2021-06-09 22:21:26 -0400

    ✨ Handles multiline text

--- src/components/InputWithLabel.js
+++ src/components/InputWithLabel.js
@@ -2,18 +2,25 @@ import React from 'react';
 import { TextInput, View, Text, StyleSheet } from 'react-native';
 import APP from '~styles/app';
 import COLORS from '~styles/colors';
-import FONTS from '~styles/fonts';
 
 function InputWithLabel(props) {
   const label = props.label;
   const description = props.description;
   const [isFocused, setIsFocused] = React.useState(false);
 
+  let height = 50;
+  let paddingVertical = 0;
+
+  if (props.multiline && props.numberOfLines) {
+    height = 50 * props.numberOfLines / 1.425;
+    paddingVertical = 15;
+  }
+
   return (
     <View style={[props.style]}>
       <Text style={[APP.inputLabel, {display: label ? 'flex' : 'none'}]}>{ label }</Text>
       <TextInput
-        style={APP.input}
+        style={[APP.input, { height, paddingVertical }]}
         keyboardType={props.keyboardType ?? 'default'}
         contextMenuHidden={props.contextMenuHidden ?? false}
         blurOnSubmit={props.disableReturn !== true}
@@ -31,6 +38,9 @@ function InputWithLabel(props) {
         secureTextEntry={props.secureTextEntry}
         autoCorrect={props.autoCorrect}
         autoCapitalize={props.autoCapitalize}
+        multiline={props.multiline}
+        numberOfLines={props.numberOfLines}
+        textAlignVertical={props.textAlignVertical}
         onFocus={() => {
           setIsFocused(true);
         }}
