3d1928da6a74db0c085518b9c7f29ac17fd02824
Brent Miller <brentwmiller93@gmail.com>
2021-07-10 23:24:55 -0400

    💄 updates add/edit ui

--- src/screens/AddRecipe.js
+++ src/screens/AddRecipe.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { SafeAreaView, View} from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import RecipeForm from '~components/RecipeForm';
 import APP from '~styles/app';
+import COLORS from '~styles/colors';
+import TYPOGRAPHY from '~styles/typography';
 
 function AddRecipeScreen({ navigation }) {
 
   return (
-    <KeyboardAwareScrollView extraHeight={ 100 }>
-      <SafeAreaView style={APP.containerWider}>
+    <KeyboardAwareScrollView extraHeight={ 100 } style={APP.container}>
+      <SafeAreaView>
+        <Text style={[TYPOGRAPHY.heading]}>Add recipe</Text>
+        <Text style={[TYPOGRAPHY.body, { marginTop: 8, color: COLORS.gray[600] }]}>Create a brand new favorite or add a classic recipe.</Text>
         <RecipeForm navigation={navigation} />
       </SafeAreaView>
     </KeyboardAwareScrollView>
--- src/screens/EditRecipe.js
+++ src/screens/EditRecipe.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { SafeAreaView, View} from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import RecipeForm from '~components/RecipeForm';
 import APP from '~styles/app';
+import COLORS from '~styles/colors';
+import TYPOGRAPHY from '~styles/typography';
 
 function EditRecipeScreen({ navigation }) {
 
   return (
-    <KeyboardAwareScrollView extraHeight={ 100 }>
-      <SafeAreaView style={APP.containerWider}>
+    <KeyboardAwareScrollView extraHeight={ 100 } style={APP.container}>
+      <SafeAreaView>
+        <Text style={[TYPOGRAPHY.heading]}>Edit recipe</Text>
+        <Text style={[TYPOGRAPHY.body, { marginTop: 8, color: COLORS.gray[600] }]}>Update your recipe to be perfect.</Text>
         <RecipeForm navigation={navigation} />
       </SafeAreaView>
     </KeyboardAwareScrollView>
