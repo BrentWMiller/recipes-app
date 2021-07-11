95840b54b2da753cf202eb4b22a6f13cf482656a
Brent Miller <brentwmiller93@gmail.com>
2021-07-10 23:37:17 -0400

    🚧 Simplifies the bottom navigation

--- src/navigation/LoggedIn.js
+++ src/navigation/LoggedIn.js
@@ -8,11 +8,26 @@ import RecipesScreen from "~screens/Recipes";
 import EditRecipeScreen from "~screens/EditRecipe";
 import AddRecipeScreen from "~screens/AddRecipe";
 import COLORS from "~styles/colors";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const navTheme = DefaultTheme;
 navTheme.colors.background = COLORS.white;
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const HomeScreenNavigator = () => {
+  return(
+    <Stack.Navigator
+      initialRouteName="Home"
+    >
+      <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Categories" component={CategoriesScreen} />
+      <Stack.Screen name="Edit Recipe" component={EditRecipeScreen} />
+    </Stack.Navigator>
+  )
+}
 
 const LoggedIn = () => {
   return (
@@ -20,15 +35,12 @@ const LoggedIn = () => {
       <Tab.Navigator
         initialRouteName="Home"
         screenOptions={{
-          cardStyle: { backgroundColor: '#fff' }
+          cardStyle: { backgroundColor: COLORS.white }
         }}
       >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-        <Tab.Screen name="Categories" component={CategoriesScreen} />
-        <Tab.Screen name="Recipes" component={RecipesScreen} />
+        <Tab.Screen name="Home" component={HomeScreenNavigator} />
         <Tab.Screen name="Add Recipe" component={AddRecipeScreen} />
-        <Tab.Screen name="Edit Recipe" component={EditRecipeScreen} />
+        <Tab.Screen name="Recipes" component={RecipesScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
